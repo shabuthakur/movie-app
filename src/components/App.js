8e238aab6a529a5ed909eec90648fbb2800c7fc0
shabuthakur <89395232+shabuthakur@users.noreply.github.com>
2022-03-31 14:46:48 +0530

    add ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import { data } from '../data'
+import Navbar from './Navbar'
+import MovieCard from'./MovieCard'
 
 function App() {
   return (
-    <div>
+    <div className='App'>
+      <Navbar/>
+      <div className='main'>
+        <div className='tabs'>
+          <div className='tab'>Movies</div>
+          <div className='tab'>Favourites</div>
+
+        </div>
+      <div className='list'>
+        {data.map(movie=>(
+          <MovieCard movie={movie}/>
+        ))}
+
+      </div>
+      </div>
+
       <h1>hii</h1>
      
     </div>
